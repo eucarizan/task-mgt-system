988d687336c78e3ee656f80c166b00355d52bb6a
eucarizan <eucarizan@gmail.com>
2024-08-17 23:59:32 +0800

    feat: user repository

---
+++ src/main/java/dev/nj/task_mgt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.nj.task_mgt.repository;
+
+import dev.nj.task_mgt.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
