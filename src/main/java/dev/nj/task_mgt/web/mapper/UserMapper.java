647ae69929b53846a6d817ad03904043a91e4eb9
eucarizan <eucarizan@gmail.com>
2024-08-17 23:55:22 +0800

    feat: user mapper

---
+++ src/main/java/dev/nj/task_mgt/web/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package dev.nj.task_mgt.web.mapper;
+
+import dev.nj.task_mgt.entities.User;
+import dev.nj.task_mgt.web.dto.NewUserDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toEntity(NewUserDto dto) {
+        return new User(
+                dto.email(),
+                dto.password());
+    }
+}
