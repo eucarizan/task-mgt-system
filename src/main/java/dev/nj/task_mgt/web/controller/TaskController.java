24e37cf2fc46f0931482ca2f2964a067f8032bf4
eucarizan <eucarizan@gmail.com>
2024-08-22 14:06:49 +0800

    refactor: task controller

--- src/main/java/dev/nj/task_mgt/web/controller/TaskController.java
+++ src/main/java/dev/nj/task_mgt/web/controller/TaskController.java
@@ -1,16 +1,35 @@
 package dev.nj.task_mgt.web.controller;
 
+import dev.nj.task_mgt.entities.Task;
+import dev.nj.task_mgt.entities.User;
+import dev.nj.task_mgt.service.TaskService;
+import dev.nj.task_mgt.web.dto.NewTicketDto;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping
-    public ResponseEntity<Void> getTasks() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<Task>> getTasks(@RequestParam Optional<String> author) {
+        return author.map(s -> ResponseEntity.ok(taskService.getTasksByAuthor(s))).orElseGet(() -> ResponseEntity.ok(taskService.getTasks()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody @Valid NewTicketDto dto,
+                                           @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(taskService.createTask(dto, user));
     }
 }
