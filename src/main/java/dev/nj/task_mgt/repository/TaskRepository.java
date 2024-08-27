453eaa28e791f35fecd09306450f5391b62b1cab
eucarizan <eucarizan@gmail.com>
2024-08-27 23:31:24 +0800

    refactor: repository extended pagingandsorting

--- src/main/java/dev/nj/task_mgt/repository/TaskRepository.java
+++ src/main/java/dev/nj/task_mgt/repository/TaskRepository.java
@@ -1,14 +1,16 @@
 package dev.nj.task_mgt.repository;
 
 import dev.nj.task_mgt.entities.Task;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
-    List<Task> findAllOrderByCreatedDesc();
+public interface TaskRepository extends CrudRepository<Task, Long>, ListPagingAndSortingRepository<Task, Long> {
+    List<Task> findAll(Sort sort);
 
     List<Task> findByAuthorIgnoreCase(String author);
 }
