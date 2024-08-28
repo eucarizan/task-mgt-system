2107c814a00885e03f3907af6a2fd1bf53eda4a5
eucarizan <eucarizan@gmail.com>
2024-08-28 22:25:02 +0800

    refactor: task slice test complete

--- src/test/java/dev/nj/task_mgt/web/TaskControllerBootTests.java
+++ src/test/java/dev/nj/task_mgt/web/TaskControllerBootTests.java
@@ -23,9 +23,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -174,10 +174,105 @@ public class TaskControllerBootTests {
         verify(userRepository).findByEmail(any(String.class));
     }
 
-    // todo-06: get tasks sorted by creation
+    @Test
+    public void getTaskSortedByCreation() throws Exception {
+        String email = "get3@test.com";
+        User user = new User(email, passwordEncoder.encode("password"));
+        List<Task> tasks = List.of(
+                new Task("task list 1", "description list 1", user),
+                new Task("task list 2", "description list 2", user),
+                new Task("task list 3", "description list 3", user)
+        );
 
-    // todo-07: get tasks by author
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(taskService.getTasks()).thenReturn(tasks);
 
+        mockMvc.perform(get("/api/tasks")
+                        .with(httpBasic("get3@test.com", "password")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].author").value("get3@test.com"))
+                .andExpect(jsonPath("$[0].status").value("CREATED"));
+
+        verify(userRepository).findByEmail(any(String.class));
+        verify(taskService).getTasks();
+    }
+
+    @Test
+    public void getTaskSortedByCreationUserDifferentThanAuthor() throws Exception {
+        String email = "get4@test.com";
+        User user = new User(email, passwordEncoder.encode("password"));
+        User authUser = new User("diff@test.com", passwordEncoder.encode("password"));
+        List<Task> tasks = List.of(
+                new Task("task list 1", "description list 1", user),
+                new Task("task list 2", "description list 2", user),
+                new Task("task list 3", "description list 3", user)
+        );
+
+        when(userRepository.findByEmail(any(String.class))).thenReturn(Optional.of(authUser));
+        when(taskService.getTasks()).thenReturn(tasks);
+
+        mockMvc.perform(get("/api/tasks")
+                        .with(httpBasic("diff@test.com", "password")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].author").value("get4@test.com"))
+                .andExpect(jsonPath("$[0].status").value("CREATED"));
+
+        verify(userRepository).findByEmail(any(String.class));
+        verify(taskService).getTasks();
+    }
+
+    @Test
+    public void getTasksByAuthor() throws Exception {
+        User user1 = new User("get5@test.com", passwordEncoder.encode("password"));
+        User user2 = new User("get6@test.com", passwordEncoder.encode("password"));
+        Task task1 = new Task("task list author 1", "description list author 1", user1);
+        Task task2 = new Task("task list author 2", "description list author 2", user2);
+        Task task3 = new Task("task list author 3", "description list author 3", user1);
+
+        when(userRepository.findByEmail("get5@test.com")).thenReturn(Optional.of(user1));
+        when(taskService.getTasksByAuthor("get5@test.com")).thenReturn(List.of(task1, task3));
+
+        mockMvc.perform(get("/api/tasks?author=get5@test.com")
+                        .with(httpBasic("get5@test.com", "password")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].author").value("get5@test.com"));
+
+        when(userRepository.findByEmail("get6@test.com")).thenReturn(Optional.of(user2));
+        when(taskService.getTasksByAuthor("get6@test.com")).thenReturn(List.of(task2));
+
+        mockMvc.perform(get("/api/tasks?author=get6@test.com")
+                        .with(httpBasic("get6@test.com", "password")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].author").value("get6@test.com"));
+
+        verify(userRepository, times(2)).findByEmail(any(String.class));
+        verify(taskService, times(2)).getTasksByAuthor(any(String.class));
+    }
+
+    @Test
+    public void getTasksByNotExistingAuthor() throws Exception {
+        User user = new User("get7@test.com", passwordEncoder.encode("password"));
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        when(taskService.getTasksByAuthor(anyString())).thenReturn(List.of());
+
+        mockMvc.perform(get("/api/tasks?author=unknown")
+                        .with(httpBasic("get7@test.com", "password")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(0)));
+
+        verify(userRepository).findByEmail(anyString());
+        verify(taskService).getTasksByAuthor(anyString());
+    }
 
     protected static String asJsonString(final Object obj) {
         try {
