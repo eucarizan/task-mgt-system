17a7abee469c6ad46c169009918aa7832b22fa08
eucarizan <eucarizan@gmail.com>
2024-09-23 23:42:57 +0800

    refactor: web security config, for token

--- src/main/java/dev/nj/task_mgt/config/WebSecurityConfig.java
+++ src/main/java/dev/nj/task_mgt/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package dev.nj.task_mgt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,23 +12,35 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    private final AccessTokenAuthenticationProvider provider;
+
+    public WebSecurityConfig(AccessTokenAuthenticationProvider provider) {
+        this.provider = provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
+
         return http
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/accounts").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/token").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/tasks").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/tasks").authenticated()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/actuator/shutdown").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                 )
+                .authenticationProvider(provider)
+                .addFilterAfter(new AccessTokenFilter(manager), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessions ->
                         sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
