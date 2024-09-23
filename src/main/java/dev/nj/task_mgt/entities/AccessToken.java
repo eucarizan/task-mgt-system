7f794ec735d5c45577d36629e7ed15785d930369
eucarizan <eucarizan@gmail.com>
2024-09-23 23:39:12 +0800

    feat: access token entities

---
+++ src/main/java/dev/nj/task_mgt/entities/AccessToken.java
@@ -0,0 +1,42 @@
+package dev.nj.task_mgt.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class AccessToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    private Date expiresAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
