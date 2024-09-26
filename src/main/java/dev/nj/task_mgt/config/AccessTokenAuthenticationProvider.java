064bf57798a113c9f0df7205aea44a4c8602c36a
eucarizan <eucarizan@gmail.com>
2024-09-27 02:06:25 +0800

    refactor: added userdetailsservice

--- src/main/java/dev/nj/task_mgt/config/AccessTokenAuthenticationProvider.java
+++ src/main/java/dev/nj/task_mgt/config/AccessTokenAuthenticationProvider.java
@@ -2,23 +2,33 @@ package dev.nj.task_mgt.config;
 
 import dev.nj.task_mgt.entities.AccessToken;
 import dev.nj.task_mgt.entities.AccessTokenAuthentication;
+import dev.nj.task_mgt.entities.User;
 import dev.nj.task_mgt.repository.TokenRepository;
+import dev.nj.task_mgt.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Component
 public class AccessTokenAuthenticationProvider implements AuthenticationProvider {
 
-    private final TokenRepository repository;
+    private final TokenRepository tokenRepository;
+    private final UserRepository userRepository;
+    private final UserDetailsService userDetailsService;
 
-    public AccessTokenAuthenticationProvider(TokenRepository repository) {
-        this.repository = repository;
+    public AccessTokenAuthenticationProvider(TokenRepository repository,
+                                             UserDetailsService userDetailsService,
+                                             UserRepository userRepository) {
+        this.tokenRepository = repository;
+        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
     }
 
     @Transactional(dontRollbackOn = BadCredentialsException.class)
@@ -26,18 +36,27 @@ public class AccessTokenAuthenticationProvider implements AuthenticationProvider
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         var token = authentication.getCredentials().toString();
 
-        AccessToken accessToken = repository
+        AccessToken accessToken = tokenRepository
                 .findByToken(token)
                 .orElseThrow(() -> new BadCredentialsException("Invalid access token"));
 
-        repository.deleteById(accessToken.getId());
-
-        if (new Date().after(accessToken.getExpiresAt())) {
+        if (LocalDateTime.now().isAfter(accessToken.getExpiresAt())) {
+            tokenRepository.deleteById(accessToken.getId());
             throw new BadCredentialsException("Invalid access token.");
         }
 
-        authentication.setAuthenticated(true);
-        return authentication;
+        User user = getUserFromAccessToken(accessToken);
+
+        AccessTokenAuthentication auth = new AccessTokenAuthentication(token);
+        auth.setUserDetails(userDetailsService.loadUserByUsername(user.getEmail()));
+        auth.setAuthenticated(true);
+
+        return auth;
+    }
+
+    private User getUserFromAccessToken(AccessToken token) {
+        return userRepository.findById(token.getUserId())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
     @Override
--- src/main/java/dev/nj/task_mgt/config/WebSecurityConfig.java
+++ src/main/java/dev/nj/task_mgt/config/WebSecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,21 +18,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final AccessTokenAuthenticationProvider provider;
+    private final AccessTokenAuthenticationProvider accessTokenAuthenticationProvider;
     private final UserDetailsService userDetailsService;
 
     public WebSecurityConfig(AccessTokenAuthenticationProvider provider, UserDetailsService userDetailsService) {
-        this.provider = provider;
+        this.accessTokenAuthenticationProvider = provider;
         this.userDetailsService = userDetailsService;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
+//        AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
+        AuthenticationManager manager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
 
         return http
                 .httpBasic(Customizer.withDefaults())
@@ -42,7 +48,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/actuator/shutdown").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                 )
-                .authenticationProvider(provider)
+                .authenticationProvider(accessTokenAuthenticationProvider)
                 .addFilterAfter(new AccessTokenFilter(manager), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessions ->
@@ -55,4 +61,14 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        daoProvider.setUserDetailsService(userDetailsService);
+        daoProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(List.of(daoProvider, accessTokenAuthenticationProvider));
+    }
+
 }
